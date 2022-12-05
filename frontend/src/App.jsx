5a6c5be955c83ce263d7645cef70685fe4454042
jvmantovas <vmsjoao@gmail.com>
2022-12-05 18:20:16 -0300

    feat(users) added logic to edit and display users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,37 @@
 import GlobalStyle from "./styles/global";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 import Container from "./styles/container";
 import Title from "./styles/title";
 import Form from "./components/Form/Form";
 import Grid from "./components/Grid/Grid";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.NOME > b.NOME ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
         <Form />
-        <Grid />
+        <Grid users={users} />
       </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.TOP_RIGHT} />
       <GlobalStyle />
