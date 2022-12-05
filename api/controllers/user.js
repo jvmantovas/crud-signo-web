9da8025398084e09f74fe33060329604e0b2d08d
jvmantovas <vmsjoao@gmail.com>
2022-12-05 18:36:53 -0300

    feat(users control): add, update, delete users

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,3 +9,35 @@ export const getUsers = (_, res) => {
     return res.status(200).json(data);
   });
 };
+
+export const addUser = (req, res) => {
+  const q = "INSET INTO usuarios(`NOME`, `EMAIL`, `TELEFONE`) VALUES(?)";
+
+  const values = [req.body.NOME, req.body.EMAIL, req.body.TELEFONE];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Formulário enviado com sucesso.");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q =
+    "UPDATE usuarios SET `NOME` = ?, `EMAIL` = ?, `TELEFONE` = ? WHERE `ID` = ?";
+
+  const values = [req.body.NOME, req.body.EMAIL, req.body.TELEFONE];
+
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Usuário atualizado com sucesso.");
+  });
+};
+
+export const deleteUser = (req, res) => {
+  const q = "DELETE FROM usuarios WHERE `ID` = ?";
+
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Usuário deleteado com sucesso.");
+  });
+};
