0780fc5abd356fa1d6eacc3934a4024d05b0a0c0
jvmantovas <vmsjoao@gmail.com>
2022-12-06 00:25:09 -0300

    feat(EmailJS) send confirmation email after submit

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Button,
   FormContainer,
@@ -11,9 +11,23 @@ import {
 } from "./styles";
 import axios from "axios";
 import { toast } from "react-toastify";
+import emailjs from "@emailjs/browser";
 
 function Form({ getUsers, onEdit, setOnEdit }) {
   const ref = useRef();
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [distric, setDistric] = useState("");
+  const [zip, setZip] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [type, setType] = useState("");
+  const [amount, setAmount] = useState("");
+  const [attractions, setAttractions] = useState("");
+  const [accept, setAccept] = useState("");
+  const [images, setImages] = useState("");
 
   useEffect(() => {
     if (onEdit) {
@@ -24,6 +38,40 @@ function Form({ getUsers, onEdit, setOnEdit }) {
       user.telefone.value = onEdit.telefone;
     }
   }, [onEdit]);
+
+  function sendEmail() {
+    const templateParams = {
+      name: name,
+      address: address,
+      distric: distric,
+      zip: zip,
+      city: city,
+      state: state,
+      email: email,
+      phone: phone,
+      type: type,
+      amount: amount,
+      attractions: attractions,
+      accept: accept,
+      images: images,
+    };
+
+    emailjs
+      .send(
+        "service_n2z8cdm",
+        "template_jrtdpnf",
+        templateParams,
+        "cGTn3qpuon0r7l6zF"
+      )
+      .then(
+        (response) => {
+          toast.success("E-mail de confirmação enviado!");
+        },
+        (err) => {
+          toast.error(err);
+        }
+      );
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -51,6 +99,7 @@ function Form({ getUsers, onEdit, setOnEdit }) {
         })
         .then(({ data }) => toast.success(data))
         .catch(({ data }) => toast.error(data));
+      sendEmail();
     }
 
     user.nome.value = "";
@@ -70,70 +119,138 @@ function Form({ getUsers, onEdit, setOnEdit }) {
       <h2>DADOS PARA ENTREGA</h2>
       <InputArea>
         <Label>Nome:</Label>
-        <Input name="nome" />
+        <Input
+          name="nome"
+          type="text"
+          placeholder="Nome completo"
+          onChange={(e) => setName(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>Endereço:</Label>
-        <Input />
+        <Input
+          name="endereço"
+          type="text"
+          placeholder="Endereço com número e complemento"
+          onChange={(e) => setAddress(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>Bairro:</Label>
-        <Input />
+        <Input
+          name="distric"
+          type="text"
+          placeholder=""
+          onChange={(e) => setDistric(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>CEP:</Label>
-        <Input />
+        <Input
+          name="zip"
+          type="number"
+          placeholder=""
+          onChange={(e) => setZip(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>Cidade:</Label>
-        <Input />
+        <Input
+          name="city"
+          type="text"
+          placeholder=""
+          onChange={(e) => setCity(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>UF:</Label>
-        <Input />
+        <Input
+          name="state"
+          type="text"
+          placeholder=""
+          onChange={(e) => setState(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>E-mail:</Label>
-        <Input name="email" type="email" />
+        <Input
+          name="email"
+          type="email"
+          placeholder="exemplo@exemplo.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>Telefone:</Label>
-        <Input name="telefone" type="number" />
+        <Input
+          name="telefone"
+          type="number"
+          placeholder=""
+          onChange={(e) => setPhone(e.target.value)}
+        />
       </InputArea>
       <h2>DADOS PARA PRODUÇÃO</h2>
       <InputArea>
         <Label>Tipo Revistinha:</Label>
-        <RadioInput id="convite" name="tipo" type="radio" value="convite" />
-        <RadioLabel for="convite">Convite</RadioLabel>
-        <RadioInput id="lembranca" name="tipo" type="radio" value="lembranca" />
-        <RadioLabel for="lembranca">Lembrança</RadioLabel>
+        <RadioInput
+          id="convite"
+          name="tipo"
+          type="radio"
+          value="convite"
+          onChange={(e) => setType(e.target.value)}
+        />
+        <RadioLabel htmlFor="convite">Convite</RadioLabel>
+        <RadioInput
+          id="lembranca"
+          name="tipo"
+          type="radio"
+          value="lembranca"
+          onChange={(e) => setType(e.target.value)}
+        />
+        <RadioLabel htmlFor="lembranca">Lembrança</RadioLabel>
         <RadioInput
           id="convite-lembranca"
           name="tipo"
           type="radio"
           value="convite-lembranca"
+          onChange={(e) => setType(e.target.value)}
         />
-        <RadioLabel for="convite-lembranca">Convite-Lembrança</RadioLabel>
+        <RadioLabel htmlFor="convite-lembranca">Convite-Lembrança</RadioLabel>
       </InputArea>
       <InputArea>
-        <Label>Quantidade:</Label>
-        <Input type="number" />
+        <Label htmlFor="number">Quantidade:</Label>
+        <Input
+          id="number"
+          type="number"
+          onChange={(e) => setAmount(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <Label>Atrações do evento:</Label>
-        <TextArea name="Atrações do evento" rows="4" cols="47" />
+        <TextArea
+          name="Atrações do evento"
+          rows="4"
+          cols="47"
+          onChange={(e) => setAttractions(e.target.value)}
+        />
       </InputArea>
       <InputArea>
         <RadioInput
           type="checkbox"
           name="Sugestões de texto para a capa"
           value="Aceito"
+          onChange={(e) => setAccept(e.target.value)}
         />
         <RadioLabel>Aceito sugestões de texto para a capa</RadioLabel>
       </InputArea>
       <InputArea>
         <Label>Imagens:</Label>
-        <RadioInput type="file" name="imagem[]" multiple="multiple" />
+        <RadioInput
+          type="file"
+          name="imagem[]"
+          multiple="multiple"
+          onChange={(e) => setImages(e.target.value)}
+        />
       </InputArea>
       <Button type="submit">Continuar</Button>
     </FormContainer>
