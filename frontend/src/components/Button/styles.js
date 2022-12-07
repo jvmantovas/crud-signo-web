f321519b99f1bb23b31cf470bdeaa0a7cd3d7158
jvmantovas <vmsjoao@gmail.com>
2022-12-07 00:41:59 -0300

    feat(styles) button component

--- frontend/src/components/Button/styles.js
+++ frontend/src/components/Button/styles.js
@@ -4,10 +4,10 @@ export const ButtonContainer = styled.button`
   height: 42px;
   padding: 10px;
   cursor: pointer;
-  width: 80%;
+  max-width: 250px;
   align-self: center;
   border-radius: 5px;
   border: none;
-  background-color: #2c73f2;
+  background-color: #565656;
   color: white;
 `;
