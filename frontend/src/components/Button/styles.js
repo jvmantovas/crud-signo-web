e847cefafa8ea951fa92e7cad63e59e97764e387
jvmantovas <vmsjoao@gmail.com>
2022-12-07 00:26:36 -0300

    feat(button) component to route

---
+++ frontend/src/components/Button/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  height: 42px;
+  padding: 10px;
+  cursor: pointer;
+  width: 80%;
+  align-self: center;
+  border-radius: 5px;
+  border: none;
+  background-color: #2c73f2;
+  color: white;
+`;
